87e176ee349490e279bed2dc04a70db6d9422b49
epicdev293 <156992896+epicdev293@users.noreply.github.com>
2024-01-23 21:34:07 -0600

    Update home.js

--- static/assets/scripts/home.js
+++ static/assets/scripts/home.js
@@ -8,7 +8,7 @@ try {
 
 if (!inFrame && !navigator.userAgent.includes('Firefox')) {
   const popup = open('about:blank', '_blank')
-  if (!popup || popup.closed) alert('Please allow popups for Aout:Blank pages!')
+  if (!popup || popup.closed) alert('Please allow popups for About:Blank pages!')
   else {
     popup.document.write(`
             <iframe src="${
